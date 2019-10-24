ed5bb004fb907ee1aa1d20dc7b9d0baf2a1d650e
Andrea Sita <andrea.sita@nttdata.com>
2019-10-24 17:12:14 +0200

    Updates

---
+++ precache-manifest.4b231d0be60f7d7fefd2cd7183727a6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880d4cdde4ede9909822aef9c6dbe562",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "02f1b3dbc71ee483c762",
+    "url": "/Pokedex/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1dd4331724ac22bb7fbb",
+    "url": "/Pokedex/static/css/main.a0ad57b1.chunk.css"
+  },
+  {
+    "revision": "02f1b3dbc71ee483c762",
+    "url": "/Pokedex/static/js/2.389e6e0f.chunk.js"
+  },
+  {
+    "revision": "1dd4331724ac22bb7fbb",
+    "url": "/Pokedex/static/js/main.4e1bab7f.chunk.js"
+  },
+  {
+    "revision": "fa511908ca5ccd18cb08",
+    "url": "/Pokedex/static/js/runtime-main.e6ae8da4.js"
+  },
+  {
+    "revision": "f2b3ff7dbad6ca2cc1dfff580ae44829",
+    "url": "/Pokedex/static/media/loader.f2b3ff7d.gif"
+  }
+]);
